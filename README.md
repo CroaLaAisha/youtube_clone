69938d974e46743980902f13da2d5240d221d465
CroaLaAisha <45094728+CroaLaAisha@users.noreply.github.com>
2021-01-23 20:42:50 +0900

    1.1 ~ #2.24 Log In and User Profile Controller (10:27)

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Youtube Cloning!
 
 Cloning Youtube width VanillaJS NodeJS
+
+## Pages:
+
+- [] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload Video
+- [ ] Video Detail
+- [ ] Edit Video
---
+++ db.js
@@ -0,0 +1,35 @@
+export const videos = [
+  {
+    id: 123,
+    title: "Video Awesome",
+    description: "This is something i love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: { id: 123, name: "Croa", email: "Croa@asd.com" },
+  },
+  {
+    id: 234,
+    title: "My girlfirend is Mermaid",
+    description: "Visual Novel",
+    views: 3445,
+    videoFile: "D:/Downloads/2B x 9S Part 04 (720).mp4",
+    creator: { id: 234, name: "Croa", email: "Croa@asd.com" },
+  },
+  {
+    id: 345,
+    title: "Video asdzxc",
+    description: "This is something i love",
+    views: 66,
+    videoFile: "https://www.youtube.com/watch?v=iKjpiOZsfQY",
+    creator: { id: 123, name: "Croa", email: "Croa@asd.com" },
+  },
+  {
+    id: 456,
+    title: "My girlfirend is Marin",
+    description: "Visual Novel",
+    views: 12312,
+    videoFile: "https://www.youtube.com/watch?v=2c2_Xno9t2g",
+    creator: { id: 234, name: "Croa", email: "Croa@asd.com" },
+  },
+];
